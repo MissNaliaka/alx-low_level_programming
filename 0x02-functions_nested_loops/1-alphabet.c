899367e18bb50f14c90b84f3f0de5d91973acb19
MissNaliaka <naliakawabuke@gmail.com>
2022-03-15 15:17:07 +0000

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,7 @@
 /**
  * print_alphabet - a function that prints alphabet in lowercase
  *
- * Return: Always 0 (Success)
- */
+*/
 void print_alphabet(void)
 {
 	int n;
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: Main.h
+ * Auth: Virginia Wabuke
+ * Desc: Header file for declarations of all functions used in 0x02-functions_nested_loops directory.
+ *
+ */
 int _putchar(char c);
 void print_alphabet(void);
