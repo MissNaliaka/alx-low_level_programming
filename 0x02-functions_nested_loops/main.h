1cf7a478de64175e3f78985b3e1de7369ec3b40c
MissNaliaka <naliakawabuke@gmail.com>
2022-03-15 15:47:33 +0000

    Task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: Main.h
+/**
+ * File: main.h
  * Auth: Virginia Wabuke
  * Desc: Header file for declarations of all functions used in 0x02-functions_nested_loops directory.
  *
  */
 int _putchar(char c);
 void print_alphabet(void);
+
+#endif
