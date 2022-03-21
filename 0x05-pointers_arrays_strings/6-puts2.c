3ab95b3d0d0a162854918491dfb3af544e781626
MissNaliaka <naliakawabuke@gmail.com>
2022-03-21 16:11:57 +0000

    task 6 please

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,19 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int count = 0;
 
-	while (*(str + i) != 0)
+	while (str[i] != 0)
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		count++;
+		i++;
+	}
+	for (i = 0; i <= count - 1 ; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
