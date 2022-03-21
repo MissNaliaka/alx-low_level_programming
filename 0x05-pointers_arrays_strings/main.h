b5b243ddcfef6dc1755c26438e2f5ebeecd74619
MissNaliaka <naliakawabuke@gmail.com>
2022-03-21 09:27:40 +0000

    add task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File: main.h
+ * Auth: Virginia Wabuke
+ * Desc: Header file for declarations of all functions for arrays and pointers
+ *
+ */
+void reset_to_98(int *n);
+
+#endif
