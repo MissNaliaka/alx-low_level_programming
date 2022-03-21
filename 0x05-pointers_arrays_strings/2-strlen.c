cc2a9e19a81100723728ef986d9f90b72a871832
MissNaliaka <naliakawabuke@gmail.com>
2022-03-21 10:06:34 +0000

    adding task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: char input
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	return (i);
+}
+
