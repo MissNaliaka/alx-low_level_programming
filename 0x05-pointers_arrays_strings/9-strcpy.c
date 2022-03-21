9269154148c790dbb13effae55e836da8fac5517
MissNaliaka <naliakawabuke@gmail.com>
2022-03-21 14:26:50 +0000

    add 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,17 +9,12 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
-	int count = 0;
 
-	while (src[i] != 0)
+	while (*(src + i))
 	{
-		count++;
+		*(dest + i) = *(src + i);
 		i++;
 	}
-	for (i = 0; i <= count; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i + 1] = 0;
+	*(dest + i) = '\0';
 	return (dest);
 }
