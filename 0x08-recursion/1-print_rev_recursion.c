e0dea86c611693d03dde7295341214c3c94d39b4
MissNaliaka <naliakawabuke@gmail.com>
2022-03-30 14:09:33 +0000

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _print_rev_recursion(char *s);
+{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
