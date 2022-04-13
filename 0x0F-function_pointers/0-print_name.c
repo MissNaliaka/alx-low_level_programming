598331ca190160207be0b9dab1c575cc38a2759f
MissNaliaka <naliakawabuke@gmail.com>
2022-04-13 15:02:05 +0000

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - entry piint from main
+ * @name: String of name
+ * @f:	Entry to another function
+ */
+void print_name(char *name, void (*f)(char *n))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
